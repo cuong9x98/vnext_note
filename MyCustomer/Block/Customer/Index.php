bc58400a9eefd5d12ea9e1c4828d72e907628a2c
cuong9x98 <cuong9x98@gmail.com>
2021-08-09 14:43:28 +0700

    update import module

---
+++ MyCustomer/Block/Customer/Index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Vnext\MyCustomer\Block\Customer;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
